9b533d88eb5852023ef6bc934ade4ae189bce163
Mark Sayers <marksayers46@icloud.com>
2020-07-19 20:50:52 -0500

    finished mvp

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,27 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+export const useForm = (initialState, key) => {
+  const [values, setValues] = useLocalStorage(
+    initialState,
+    key,
+  );
+
+  const handleChanges = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const clearForm = () => {
+    setValues(initialState);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    clearForm();
+  };
+
+  return [values, handleChanges, clearForm, handleSubmit]
+}
